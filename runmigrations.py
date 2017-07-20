60ba1e25bc5956a0003dd38709adb1626c39c040
tomerco25 <tomerco25@gmail.com>
2017-07-20 15:22:14 +0300

    1

--- runmigrations.py
+++ runmigrations.py
@@ -1,11 +1,10 @@
 import os
 import subprocess
-userfolderwork='C:\Users\Tomerco\Desktop\python\seleniummm\myproject'
-userfolderhome='C:/Users/tomer-i5-PC/Desktop/myproject/'
+if "i5" in os.path.expandvars("%userprofile%"):
+    userfolder='C:/Users/tomer-i5-PC/Desktop/myproject/'
+else:
+    userfolder = 'C:\Users\Tomerco\Desktop\python\seleniummm\myproject'
 
-subprocess.call('python {}/manage.py makemigrations'.format(userfolderwork))
-subprocess.call('python {}/manage.py migrate'.format(userfolderwork))
-
-# os.system('start python C:/Users/tomer-i5-PC/Desktop/myproject/manage.py makemigrations')
-# os.system('start python C:/Users/tomer-i5-PC/Desktop/myproject/manage.py migrate')
-
+if __name__ == '__main__':
+    subprocess.call('python {}/manage.py makemigrations'.format(userfolder))
+    subprocess.call('python {}/manage.py migrate'.format(userfolder))
--- runserver.py
+++ runserver.py
@@ -1,7 +1,4 @@
 import os
-userfolderwork='C:\Users\Tomerco\Desktop\python\seleniummm\myproject'
-userfolderhome='C:/Users/tomer-i5-PC/Desktop/myproject/'
-
-
-os.system('start python {}/manage.py runserver'.format(userfolderwork))
+from runmigrations import userfolder
+os.system('start python {}/manage.py runserver'.format(userfolder))
 
